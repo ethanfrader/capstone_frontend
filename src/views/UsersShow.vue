c5e47da39040a4dbd516ce9182ff234152df5019
Ethan Rader <rader.ethan@gmail.com>
2020-05-11 19:37:06 -0500

    apply theme to user profile

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="home">
-    <div>
-      <h1>{{ user.first_name }} {{ user.last_name}}</h1>
-      <img :src="user.profile_picture" height=100px>
-      <div v-if="user.id === currentUser.id">
-        <button>Edit info</button>
-      </div>
-      <h2>{{user.first_name}}'s artist pages:</h2>
-      <div v-if="user.artists.length === 0">
-          <h3>You don't have any linked artists!</h3>
-      </div>
-      <div v-for="artist in user.artists">
-        <h3>{{ artist.name }}</h3>
-        <router-link :to="`/artists/${artist.id}`">Go to page</router-link>
-      </div>
+  <div class="container">
+    <section>
+      <div>
+        <div class="profile-left">
+          <h1>{{ user.first_name }} {{ user.last_name}}</h1>
+          <img :src="user.profile_picture">
+          <div v-if="user.id === currentUser.id">
+            <button class="btn btn-primary">Edit info</button>
+          </div>
+        </div>
+        <div class="profile-right">
+          <h2>{{user.first_name}}'s artist pages:</h2>
+          <div v-if="user.artists.length === 0">
+            <h3>You don't have any linked artists!</h3>
+          </div>
+          <div v-for="artist in user.artists">
+            <h3>{{ artist.name }}</h3>
+            <router-link id="user-link" :to="`/artists/${artist.id}`">Go to page</router-link>
+          </div>
+        </div>
     </div>
     <br>
+    </section>
   </div>
 </template>
 
