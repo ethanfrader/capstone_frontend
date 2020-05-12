81e96580142a91cdae589886c1c17bafdd8eff62
Ethan Rader <rader.ethan@gmail.com>
2020-05-11 21:33:14 -0500

    update look of posts index

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -3,17 +3,26 @@
     <section class="page-section">
       <div class="text-center">
         <div>
-          <router-link class="btn btn-accent btn-xl" to="/posts-new" id="add-post">Add Post</router-link>
+          <router-link class="btn btn-xl btn-accent" to="/posts-new" id="add-post">Add Post</router-link>
         </div>
       </div>
     <h1>Recent posts:</h1>
 
     <div v-for="post in posts">
       <div id="post-container">
-        <h4>{{post.artist.name}}: </h4><p>{{post.text}}</p>
-        <small>Posted at {{post.created_at}}</small>
-        <div>
+        <div class="post-image">
+        </div>
+        <div class="post-body">
+          <a class="bold-link" :href="`/artists/${post.artist.id}`">
+          <h4>{{post.artist.name}}: </h4>
+          </a>
+          <div class="post-text">
+            <p>{{post.text}}</p>
+            <small>Posted at {{post.created_at}}</small>
+          </div>
+          <div>
           <button class="btn btn-dark">Send them a message</button>
+        </div>
         </div>
       </div>
     </div>
