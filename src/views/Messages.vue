b820125c77af7ed97f8c167be4a009405e0741c4
Ethan Rader <rader.ethan@gmail.com>
2020-05-17 10:51:58 -0500

    filter out duplicate message threads

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -11,16 +11,21 @@
             <select name="basic-dropdown" v-model="artist">
               <option v-for="artist in allArtists" :value="artist">{{ artist.name }}</option>
             </select>
-            <label for="basic-dropdown">Conversation with:</label>
-            <select name="basic-dropdown" v-model="receivingArtist">
-              <option v-for="receivingArtist in receivingArtists" :value="receivingArtist">
-                {{ receivingArtist.name }}
-              </option>
-            </select>
+            
           </div>
           <!-- <input type="submit" class="btn btn-primary" value="Submit"> -->
+          <button v-on:click="openConversations()">Open Conversations</button>
         </form>
-        <button v-on:click="openThread()">Open Thread</button>
+
+        <div class="filter" v-if="isReady == true && artist.name">
+          <label for="basic-dropdown">Conversation with:</label>
+            <select name="basic-dropdown" v-model="receivingArtistName">
+              <option v-for="convo in allConversationThreads" :value="convo">
+                {{ convo }}
+              </option>
+            </select>
+          <button v-on:click="seeThread()">See Thread</button>
+        </div>
 
         <div v-if="artist.id && receivingArtist.id">
           <h4>{{ artist.name }}'s conversation with {{ receivingArtist.name }}:</h4>
@@ -36,7 +41,7 @@
         </div>
         <br />
 
-        <div v-for="message in messages">
+        <div v-for="message in conversationMessages">
           <div
             v-if="message.artist.id === artist.id && message.recipient.id === receivingArtist.id"
             class="sent-message"
@@ -77,12 +82,17 @@ export default {
       allArtists: [],
       artist: {},
       artistMessages: [],
-      receivingArtist: {},
+      receivingArtist: "",
       receivingArtists: [],
       condensedConversations: [],
       messageText: "",
       sendTo: {},
       currentThread: [],
+      allParticipants: [],
+      allConversationThreads: [],
+      isReady: false,
+      receivingArtistName: "",
+      conversationMessages: [],
     };
   },
   created: function() {
@@ -92,6 +102,8 @@ export default {
         console.log(response.data);
         var receivers = [];
         this.messages = response.data;
+        console.log("messages: ");
+        console.log(this.messages);
         this.messages.forEach(function(message) {
           console.log(message.recipient);
           receivers.push(message.recipient);
@@ -99,8 +111,14 @@ export default {
         this.receivingArtists = receivers;
         console.log("receiving artists:");
         console.log(this.receivingArtists);
-        this.condensedConversations = [...new Set(this.receivingArtists.map(x => x.name))];
-        console.log(this.condensedConversations);
+        var participants = [];
+        this.messages.forEach(function(message) {
+          participants.push(message.recipient.name);
+          participants.push(message.artist.name);
+        });
+        this.allParticipants = participants;
+        console.log("all participants");
+        console.log(this.allParticipants);
       })
       .catch(error => {
         console.log(error);
@@ -134,21 +152,56 @@ export default {
           console.log(error);
         });
     },
-    openThread: function() {
-      var params = {
-        artist_id: this.artist.id,
-      };
-      console.log("params being sent: ");
-      console.log(params);
+    openConversations: function() {
+      var myArtistName = this.artist.name;
+      console.log(myArtistName);
+      if (myArtistName !== undefined) {
+        this.isReady = !this.isReady;
+        console.log("flipped isReady");
+      }
       axios
-        .get("/api/messages", params)
+        .get("/api/messages")
         .then(response => {
-          this.currentThread = response.data;
-          console.log(response.data);
+          // this.currentThread = response.data;
+          // console.log(response.data);
+          var nonUserParticipants = [];
+          console.log(myArtistName);
+          this.allParticipants.forEach(function(participant) {
+            console.log(participant);
+            console.log("participant:");
+            if (participant !== myArtistName) {
+              nonUserParticipants.push(participant);
+            }
+          });
+          console.log("non user participants");
+          console.log(nonUserParticipants);
+          var allConversationists = [...new Set(nonUserParticipants)];
+          console.log("all conversationists");
+          console.log(allConversationists);
+          this.allConversationThreads = allConversationists;
         })
         .catch(error => {
           console.log(error);
         });
+    },
+    seeThread: function() {
+      var messagesPlaceholder = [];
+      var i = 0;
+      while (i < this.messages.length) {
+        if (this.messages[i].artist.name === this.artist.name || this.messages[i].recipient.name === this.artist.name) {
+          messagesPlaceholder.push(this.messages[i]);
+        }
+        i += 1;
+      }
+      if (this.messages[0].artist.id === this.artist.id) {
+        this.receivingArtist = this.messages[0].recipient;
+      }
+      if (this.messages[0].recipient.id === this.artist.id) {
+        this.receivingArtist = this.messages[0].artist;
+      }
+      this.conversationMessages = messagesPlaceholder;
+      console.log("refined converstaion messages");
+      console.log(this.conversationMessages);
     },
   },
 };
